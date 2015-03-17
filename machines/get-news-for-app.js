dcbf25a2a7cad1b03d365ca39d0d4ea75ba49b34
Shern Shiou Tan <shernshiou@gmail.com>
2015-03-17 23:49:55 +0800

    Add get steam news thru http get

---
+++ machines/get-news-for-app.js
@@ -0,0 +1,78 @@
+module.exports = {
+
+  friendlyName: 'Get News for Steam app',
+
+  description: 'Fetch news feeds for each Steam game',
+
+  extendedDescription: '',
+
+  inputs: {
+    appid: {
+      description: 'AppID of the game you want the news of.',
+      example: 400,
+      required: true
+    },
+    count: {
+      description: 'How many news entries you want to get returned.',
+      example: 3
+    },
+    maxlength: {
+      description: 'Maximum length of each news entry.',
+      example: 300
+    }
+  },
+
+  defaultExit: 'success',
+
+
+  exits: {
+
+    error: {
+      description: 'Unexpected error occurred.',
+    },
+
+    success: {
+
+      description: '',
+
+      example:  {
+        appnews: {
+          appid: 440,
+          newsitems: [
+          ]
+        }
+      }
+    }
+
+  },
+
+  fn: function (inputs,exits) {
+
+    var Http = require('machinepack-http');
+
+    Http.sendHttpRequest({
+      baseUrl: 'http://api.steampowered.com/',
+      url: 'ISteamNews/GetNewsForApp/v0002/',
+      method: 'get',
+      params: {
+        appid: inputs.appid,
+        count: inputs.count,
+        maxlength: inputs.maxlength,
+        format: 'json'
+      }
+    }).exec({
+      success: function (res) {
+        var response = JSON.parse(res.body);
+        console.log(response);
+        console.log(JSON.stringify(response));
+        return exits.success(response);
+      },
+
+      error: function (err) {
+        return exits.error(err);
+      }
+    })
+
+  }
+
+};
