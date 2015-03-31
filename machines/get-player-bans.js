383cc0b23b144575fe985a4e9568467e567b0626
Shern Shiou Tan <shernshiou@gmail.com>
2015-03-31 13:40:37 +0800

    Get player bans machine

--- machines/get-player-bans.js
+++ machines/get-player-bans.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-  friendlyName: 'Get player summaries',
+  friendlyName: 'Get player bans',
 
-  description: 'Returns basic profile information for a list of 64-bit Steam IDs.',
+  description: 'GetPlayerBans returns Community, VAC, and Economy ban statuses for given players.',
 
   extendedDescription: '',
 
@@ -37,23 +37,12 @@ module.exports = {
       example: {
         players: [
           {
-            steamid: "76561197960435530",
-            communityvisibilitystate: 3,
-            profilestate: 1,
-            personaname: "Robin",
-            lastlogoff: 1426915786,
-            profileurl: "http://steamcommunity.com/id/robinwalker/",
-            avatar: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4.jpg",
-            avatarmedium: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4_medium.jpg",
-            avatarfull: "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4_full.jpg",
-            personastate: 0,
-            realname: "Robin Walker",
-            primaryclanid: "103582791429521412",
-            timecreated: 1063407589,
-            personastateflags: 0,
-            loccountrycode: "US",
-            locstatecode: "WA",
-            loccityid: 3961
+            SteamId: "76561197960435530",
+            CommunityBanned: false,
+            VACBanned: false,
+            NumberOfVACBans: 0,
+            DaysSinceLastBan: 0,
+            EconomyBan: "none"
           }
         ]
       }
@@ -61,13 +50,14 @@ module.exports = {
 
   },
 
+
   fn: function (inputs, exits) {
 
     var Http = require('machinepack-http');
 
     Http.sendHttpRequest({
       baseUrl: 'http://api.steampowered.com/',
-      url: 'ISteamUser/GetPlayerSummaries/v0002/',
+      url: 'ISteamUser/GetPlayerBans/v1/',
       method: 'get',
       params: {
         steamids: inputs.steamids.join(),
@@ -77,7 +67,7 @@ module.exports = {
     })
     .exec({
       success: function (res) {
-        var response = JSON.parse(res.body).response;
+        var response = JSON.parse(res.body);
         return exits.success(response);
       },
       error: function (err) {
